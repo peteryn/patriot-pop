442c0ece8375c26f40eed7d41745b67224d6e77e
Peter Yuan <peter.yuan18@gmail.com>
2024-04-12 16:45:27 -0400

    Remove unneeded add dj modal from js

--- public/js/modal.js
+++ public/js/modal.js
@@ -16,12 +16,6 @@ document.addEventListener("DOMContentLoaded", function () {
     modal.style.display = "none";
   };
 
-  const addDjBtn = document.querySelector("#add-dj-btn");
-  const addModal = document.querySelector("#add-dj-modal");
-  addDjBtn.onclick = function () {
-    addModal.style.display = "block";
-  };
-
   const addModalCancel = document.querySelector("#add-dj-btn-cancel");
   addModalCancel.onclick = function () {
     addModal.style.display = "none";
@@ -34,9 +28,6 @@ document.addEventListener("DOMContentLoaded", function () {
   window.onclick = function (event) {
     if (event.target == modal) {
       modal.style.display = "none";
-    }
-    if (event.target == addModal) {
-      addModal.style.display = "none";
     }
   };
 
